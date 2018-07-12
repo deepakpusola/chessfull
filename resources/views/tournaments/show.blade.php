28e62a035246a1b2b33b4cd4b04158944d0b1a61
Trumpets <operations@trumpetstechnologies.com>
2018-07-12 17:27:06 +0530

    close tournament

--- resources/views/tournaments/show.blade.php
+++ resources/views/tournaments/show.blade.php
@@ -32,6 +32,13 @@
 
         <div class="col-md-8">
 
+          @if($tournament->is_live)
+            
+            <p class="badge badge-warning" style="font-size: 22px;">Ends in : <span id="endtimer"></span></p>
+
+          @endif
+
+
           @if(auth()->user()->isEnrolled($tournament))
             <div class="card mb-4">
                 <div class="card-header" style="font-size: 22px;">My Matches</div>
@@ -128,3 +135,42 @@
     </div>
 </div>
 @endsection
+
+
+@section('scripts')
+  
+
+ <script>
+// Set the date we're counting down to
+var countDownDate = new Date("{{ $tournament->endtime }}").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get todays date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now an the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("endtimer").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("endtimer").innerHTML = "EXPIRED";
+  }
+}, 1000);
+</script>
+
+
+@endsection
+
