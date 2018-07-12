3a6b7ec4bd9af402ec960d835073d156cf5a5b59
Trumpets <operations@trumpetstechnologies.com>
2018-07-12 17:46:26 +0530

    close tournament

--- app/Http/Controllers/TournamentsController.php
+++ app/Http/Controllers/TournamentsController.php
@@ -34,7 +34,7 @@ class TournamentsController extends Controller
         
     	$tournament->is_live = $tournament->starttime <= \Carbon\Carbon::now('Asia/Kolkata');
     	$matches = $tournament->matches()->where('player_1', auth()->id())->orWhere('player_2', auth()->id())->get();
-    	dd($tournament->matches);
+    	dd($tournament);
         return view('tournaments.show', compact('tournament', 'matches'));
     }
 
