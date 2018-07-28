efe532c78b4b755a50762b6377869c4d90613b1d
Trumpets <operations@trumpetstechnologies.com>
2018-07-28 10:00:36 +0530

    bugs

--- resources/views/tournaments/show.blade.php
+++ resources/views/tournaments/show.blade.php
@@ -192,6 +192,7 @@
  <script>
 // Set the date we're counting down to
 var countDownDate = new Date("{{ $tournament->endtime }}").getTime();
+var startDownDate = new Date("{{ $tournament->start }}").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -201,6 +202,12 @@ var x = setInterval(function() {
 
   // Find the distance between now an the count down date
   var distance = countDownDate - now;
+  var startDistance = startDownDate - now;
+
+  if(startDistance < 0)
+  {
+    window.reload();
+  }
 
   // Time calculations for days, hours, minutes and seconds
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
