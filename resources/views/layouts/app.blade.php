0c8c0d6fb8c9f78a5d2cfe15c068478781150412
Trumpets <operations@trumpetstechnologies.com>
2018-08-06 12:58:02 +0530

    upload file

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,11 +36,11 @@
                 <a class="navbar-brand" href="{{ url('/') }}">
                    <img src="/img/logo.png" style="width: 190px;margin-top: 13px;">
                 </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
-                </button>
+                </button> -->
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <div class="" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
 
